ab99b1f7c147a66eeaef1863aba59b6dcad31fee
Dan Simon <dan@tumblr.com>
2016-11-16 11:53:29 -0500

    rapidoid - setting maxPipeline to 16

--- frameworks/Java/rapidoid/src/main/java/lowlevel/Main.java
+++ frameworks/Java/rapidoid/src/main/java/lowlevel/Main.java
@@ -1,8 +1,11 @@
 package lowlevel;
 
+import org.rapidoid.config.Conf;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		Conf.HTTP.set("maxPipeline", 16);
 		new PlaintextAndJsonServer().listen(8080);
 	}
 
